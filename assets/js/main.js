c31aa8d47e3d734d33d44d4bdfcafb2646081288
speedbuild98 <lautaropectm98@gmail.com>
2022-05-12 12:15:22 -0300

    Se agregó linea de skills

--- assets/js/main.js
+++ assets/js/main.js
@@ -133,3 +133,4 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+/*==================== CARRUSEL ====================*/ 
