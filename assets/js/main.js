5a5a7c500bb721689daddaf3283a9dabde6edada
speedbuild98 <lautaropectm98@gmail.com>
2022-05-13 15:47:46 -0300

    Se agregó slider de certificados

--- assets/js/main.js
+++ assets/js/main.js
@@ -151,5 +151,6 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 3000); // Change image every 2 seconds
+  setTimeout(showSlides, 1000); // Change image every 2 seconds
 }
+
